61866b25d75f49e5be6aa88622c0919d5b9ed91b
500beckwon <kjsx60@gmail.com>
2023-01-12 19:28:48 +0900

    Decoding Entities 제작

---
+++ Bemily/Entities/AppInfo.swift
@@ -0,0 +1,35 @@
+//
+//  AppInfo.swift
+//  Bemily
+//
+//  Created by ByungHoon Ann on 2023/01/10.
+//
+
+import Foundation
+
+struct AppInfo: Codable {
+    let screenshotList: [String]
+    
+    struct AppDetail: Codable {
+        let screenshotUrls: [String]
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case screenshotList = "results"
+    }
+}
+
+extension AppInfo {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        guard let detail = try container
+            .decode([AppDetail].self,
+                    forKey: .screenshotList)
+                .first?
+            .screenshotUrls else {
+            self.screenshotList = []
+            return
+        }
+        self.screenshotList = detail
+    }
+}
