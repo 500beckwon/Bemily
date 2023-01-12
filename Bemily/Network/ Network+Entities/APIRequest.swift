6d14af2e70c4ccfd08f2551746ae6c8bec4f4401
500beckwon <kjsx60@gmail.com>
2023-01-12 19:47:11 +0900

    APIRequest 제작

---
+++ Bemily/Network/ Network+Entities/APIRequest.swift
@@ -0,0 +1,38 @@
+//
+//  APIRequest.swift
+//  Bemily
+//
+//  Created by ByungHoon Ann on 2023/01/10.
+//
+import Foundation
+
+protocol APIRequest {
+    var baseURLString: String { get }
+    var urlString: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var parameters: [String : String]? { get }
+    var headers: [String: String]? { get }
+}
+
+extension APIRequest {
+    var baseURLString: String {
+        return "https://itunes.apple.com/kr"
+    }
+    
+    var urlString: String {
+        return baseURLString
+    }
+
+    var method: HTTPMethod {
+        return .get
+    }
+    
+    var parameters: [String : String]? {
+        return nil
+    }
+    
+    var headers: [String: String]? {
+        return ["Content-type": "application/json"]
+    }
+}
