128ccf558e842274df032ee5a6ab6af50103f521
500beckwon <kjsx60@gmail.com>
2023-01-15 16:53:50 +0900

    Init Detail Bemily Manager

--- Bemily/Presentation/DetailBemilyManager.swift
+++ Bemily/Presentation/DetailBemilyManager.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+final class DetailBemilyManager {
+    private var screenShotList = [String]()
+    private var currentIndex = 0
+    
+    func requestFetchDetailBemily(id: String,
+                                  completion: @escaping(_ screenshot: String?) -> Void) {
+        AppInfoRequest.requestAppInfo(id: id) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let success):
+                self.screenShotList = success.screenshotList
+                self.currentIndex += 1
+                completion(success.screenshotList.first)
+            case .failure(let failure):
+                print(failure.localizedDescription)
+                completion(nil)
+            }
+        }
+    }
+    
+    func nextScreenShot() -> String? {
+        guard screenShotList.count > currentIndex else { return nil }
+        let screenShot = screenShotList[currentIndex]
+        currentIndex += 1
+        return screenShot
+    }
+}
